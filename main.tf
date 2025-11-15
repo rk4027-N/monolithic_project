81de3b6342fdcf0a0b181a92d82fcb73c9714437
root <root@ip-172-31-80-136.ec2.internal>
2025-11-15 07:41:30 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "aws_launch_template" "web_server_as" {
+    name = "myproject"
+    image_id           = "ami-0cae6d6fe6048ca2c"
+    vpc_security_group_ids = [aws_security_group.web_server.id]
+    instance_type = "t3.micro"
+    key_name = "mono"
+    tags = {
+        Name = "DevOps"
+    }
+    
+}
+   
+
+
+  resource "aws_elb" "web_server_lb"{
+     name = "web-server-lb"
+     security_groups = [aws_security_group.web_server.id]
+     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     listener {
+      instance_port     = 8000
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    tags = {
+      Name = "terraform-elb"
+    }
+  }
+resource "aws_autoscaling_group" "web_server_asg" {
+    name                 = "web-server-asg"
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
+    health_check_type    = "EC2"
+    load_balancers       = [aws_elb.web_server_lb.name]
+    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    launch_template {
+        id      = aws_launch_template.web_server_as.id
+        version = "$Latest"
+      }
+    
+    
+  }
+
