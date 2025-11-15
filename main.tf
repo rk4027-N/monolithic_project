a0b5603b38112a842b28acff39c4d28fefb078e9
Rajkumar <gudalarajkumar4444@gmail.com>
2025-11-15 13:19:40 +0530

    Change ELB subnets in main.tf
    Updated ELB subnets to new values.

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-0ec1fc46b62624dba", "subnet-066e3dca4e51dac8f"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
